14ac24b9878acb74ba2481344229cf4c5aaf6660
VandyckJ <r0894869@student.thomasmore.be>
2024-02-03 20:50:35 +0100

    2.24 done

--- ZTM Vue Course Code/Section 2/section-2-1-STARTER/app.js
+++ ZTM Vue Course Code/Section 2/section-2-1-STARTER/app.js
@@ -2,15 +2,27 @@ const vm = Vue.createApp({
     data() {
         return{
             firstName: 'John',
+            middleName: '',
             lastName: 'Doe',
             url: 'https://google.com',
-            raw_url: '<a href="https://google.com" target="_blank" >Google</a>'
+            raw_url: '<a href="https://google.com" target="_blank" >Google</a>',
+            age: 20,
         }
     },
     methods: {
         fullName(){
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
-        }
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        },
+        increment(){
+            this.age++
+        },
+        updateLastName(message,event){
+            console.log(message)
+            this.lastName = event.target.value
+        },
+        updateMiddleName(event){
+            this.middleName = event.target.value
+        },
     }
 }).mount('#app')
 
