5668c0a08f2957dd8fa2cbe25018e8b0ef37a95e
VandyckJ <r0894869@student.thomasmore.be>
2024-02-07 22:20:47 +0100

    Section 7

---
+++ ZTM Vue Course Code/Section 6/dynamic-components/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <select v-model="componentName">
+    <option value="home">Home</option>
+    <option value="about">About</option>
+  </select>
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
+</template>
+<script>
+import home from "./components/home.vue"
+import about from "./components/about.vue"
+export default{
+  name:'App',
+  components:{
+    home,
+    about,
+  },
+  data() {
+    return {
+      componentName: "home",
+    }
+  }
+}
+</script>
---
+++ ZTM Vue Course Code/Section 6/dynamic-components/src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Home</h3>
+</template>
+
+<script>
+export default {
+    unmounted(){
+        console.log("Unmounted")
+    },
+    activated(){
+        console.log("Activated")
+    },
+    deactivated(){
+        console.log("Deactivated")
+    },
+};
+</script>
