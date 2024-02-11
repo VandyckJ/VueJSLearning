e290e8edc011d8baf46e9e635b8cbbc1f461ce68
VandyckJ <r0894869@student.thomasmore.be>
2024-02-11 12:56:29 +0100

    Section 22
    First Zip files were outdated, had to take new starters

---
+++ ZTM Vue Course Code/Section 22/section-22-5-STARTER/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <app-action />
+  </div>
+</template>
+
+<script>
+import AppAction from "@/components/Action.vue";
+export default {
+  name: "App",
+  components: {
+    AppAction,
+  },
+};
+</script>
+<style src="bootstrap/dist/css/bootstrap.css"></style>
