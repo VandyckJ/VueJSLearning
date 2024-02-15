b8714faef9cf5078d80650cf525e802e933f6194
VandyckJ <r0894869@student.thomasmore.be>
2024-02-15 19:36:41 +0100

    Bugfix individual song

--- ZTM Vue Course Code/musicProject/src/router/index.js
+++ ZTM Vue Course Code/musicProject/src/router/index.js
@@ -4,7 +4,7 @@ import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
 import useUserStore from '@/stores/user'
-import Song from '@/stores/user'
+import Song from '@/views/Song.vue'
 const routes = [
   {
     name: 'home',
